cd0749102296f28f2c9d8d8ccaf244fc65e66e1c
juliajaca <juliajaca@hotmail.com>
2020-08-07 03:50:13 +0200

    casillas con color, con ID, y draggables

--- app.js
+++ app.js
@@ -7,10 +7,24 @@ document.addEventListener('DOMContentLoaded', ()=>{
     // esto lo pone ania pero no veo que sirva
     // const squares = []
 
+    const candyColors = [
+        'red',
+        'yellow',
+        'orange',
+        'purple',
+        'green',
+        'blue'
+    ]
+
+
     //Create board
     function createBoard(){
         for (let i = 0; i < width*width; i++){
             const square = document.createElement('div')
+            let randomColor = Math.floor(Math.random() * candyColors.length)
+            square.style.backgroundColor = candyColors[randomColor]
+            square.setAttribute('draggable', true)
+            square.setAttribute('id', i)
             grid.appendChild(square)
             // esto lo pone Ania pero no veo que sirva
             // squares.push(square)

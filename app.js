20d826cc470cf0f3292a82500967fb8d128e081c
juliajaca <juliajaca@hotmail.com>
2020-08-07 03:14:12 +0200

    creación del tablero con las casillas

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', ()=>{ 
-    console.log('hola')
-    console.log('ey')
+    // console.log('hola')
+    // console.log('ey')
+    const grid = document.querySelector('.grid')
+    // console.log(grid)
+    const width = 8
+    // esto lo pone ania pero no veo que sirva
+    // const squares = []
+
+    //Create board
+    function createBoard(){
+        for (let i = 0; i < width*width; i++){
+            const square = document.createElement('div')
+            grid.appendChild(square)
+            // esto lo pone Ania pero no veo que sirva
+            // squares.push(square)
+        }
+    }
+    createBoard()
 })
